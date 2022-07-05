a7fdff4fcd3055d003a4c3d8093e06e83f5c0521
heinz0Jb <henock0kifle@gmail.com>
2022-07-05 15:22:16 -0700

    0. _putchar

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,48 @@
+#include <stdio.h>                                                                                      
+
+                                                                                                        
+
+/**                                                                                                     
+
+                                                                                                        
+
+  *main - Entry point                                                                                   
+
+                                                                                                        
+
+  Return: Always 0 (Success)                                                                            
+
+                                                                                                        
+
+   */                                                                                                   
+
+                                                                                                        
+
+int main(void)                                                                                          
+
+                                                                                                        
+
+{                                                                                                       
+
+        puts("\"Programming is like building a ult                                                      
+
+                                                                                                        
+
+root@13f3851bb6ae:/alx-low_level_programming/0x00-hello_world#                                          
+
+root@13f3851bb6ae:/alx-low_level_programming/0x00-hello_world# cat > 4*                                 
+
+#include <stdio.h>                                                                                      
+
+/**                                                                                                     
+
+  * main - Entry point                                                                                  
+
+  * Return: Always 0 (Success)                                                                          
+
+   */                                                                                                   
+
+int main(void)                                                                                          
+
+{                                                                                                       
+
