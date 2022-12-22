acde63e9b225f91a350c1fc2d629b775a5d1eb8c
heinz0Jb <henock0kifle@gmail.com>
2022-12-22 04:07:48 -0800

    print alphabet in SL

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -4,11 +4,11 @@
  * return - returns 0
  */
 
-void print_alphabet( void)
+void print_alphabet(void)
 {
 	char letter;
 
-	for(letter = 'a'; letter<= 'z'; letter++)
+	for (letter = 'a'; letter <= 'z'; letter++)
 	{
 		_putchar(letter);
 	}
