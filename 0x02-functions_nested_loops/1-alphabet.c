421d663738cc929acec57540d674f51465ebf542
heinz0Jb <henock0kifle@gmail.com>
2022-12-21 23:12:22 -0800

    print alphabet in SL

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,18 +1,10 @@
 #include <stdio.h>
+#include"main.h"
 /**
   * main -  Entry point
   * print_alphabet - prints lowercase alphabet
    */
-void print_alphabet(void)
-{
-	int i=97;
-	while(i<123)
-	{
-		putchar(i);
-		putchar('\n');
-		i++;
-	}
-}
+
 int main(void)
 {
 	print_alphabet();
