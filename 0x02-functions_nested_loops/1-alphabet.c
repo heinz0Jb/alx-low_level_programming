d69d9bbbc85bc39ec23a4886f7ffc4d5034bad49
heinz0Jb <henock0kifle@gmail.com>
2022-12-22 04:04:55 -0800

    print alphabet in SL

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,12 +1,13 @@
 #include "main.h"
 /*
- * print_alphabet - prints alphabet
- * return - no return
+ * print_alphabet - prints alphabet in small letter
+ * return - returns 0
  */
 
 void print_alphabet( void)
 {
 	char letter;
+
 	for(letter = 'a'; letter<= 'z'; letter++)
 	{
 		_putchar(letter);
