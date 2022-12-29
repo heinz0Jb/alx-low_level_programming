046325adbf16d965900fbe88d12883544a5fc8b6
heinz0Jb <henock0kifle@gmail.com>
2022-12-28 23:13:44 -0800

    isower

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -5,15 +5,11 @@
  * return - 1 if c is lowercase otherwise 0
  */
 int _islower (int c)
-	char a;
-	a = (char)c;
-{
+	int a = '0' + c;
+	{
 	if( a  >= 97 && a <= 122)
 	{
 		return(1);
 	}
-	else
-	{
-		return(0);
-	}
-}
+	else{
+	
