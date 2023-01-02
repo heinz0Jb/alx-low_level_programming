e0757bf5c04df7c9e7f71124d90b9bdaa0d1aeda
heinz0Jb <henock0kifle@gmail.com>
2023-01-02 12:07:14 -0800

    memory copy

---
+++ 0x07-pointers_arrays_strings/_putchar.c
@@ -0,0 +1,12 @@
+#include "main.h"
+#include <unistd.h>
+
+/*
+ * _putchar - writes the character c to stdout
+ * Return: On success 1 pr -1 on error
+ *
+ */
+int _putchar(char c)
+{
+	return (write(1, &c, 1);
+}
