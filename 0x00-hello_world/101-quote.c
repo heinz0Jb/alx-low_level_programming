96c36718416dbd0cefd5ad0cf8fc90ce13f7d983
heinz0Jb <henock0kifle@gmail.com>
2023-04-04 01:16:39 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,15 +1,13 @@
-#include <unistd.h> 
+#include <unistd.h>
 #include <sys/syscall.h>
 
 /**
  * main - entry point
  *
- * return: always 0
  */
 
 int main(void)
 {
-	const char msg[] = "and that piece of art is useful\" - Dora Korpar,2015-10-19,' \n"; 
-    syscall(SYS_write, 1, msg, sizeof(msg)); 
-    return 0;
+	const char msg[] = "and that piece of art is useful\" - Dora Korpar,2015-10-19 \n"; 
+    syscall(SYS_write, 1, msg, sizeof(msg));
 }
