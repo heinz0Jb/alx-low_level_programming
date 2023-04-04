c2e26962a3ad2d66c70387aa2f6d816918437bea
heinz0Jb <henock0kifle@gmail.com>
2023-04-04 00:55:05 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -8,5 +8,7 @@
 
 int main(void)
 {
-	system("echo and that piece of art is useful"" - Dora Korpar," 2015-10-19");
+	const char msg[] = "and that piece of art is useful" + " - Dora Korpar," + 2015-10-19\n"; 
+    syscall(SYS_write, 1, msg, sizeof(msg)); 
+    return 0;
 }
