ebc349cd597945d75a010d7391a4122d5820f0f6
heinz0Jb <henock0kifle@gmail.com>
2023-04-04 14:02:06 -0700

    9-strcpy.c

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -8,7 +8,7 @@
  *
  * Return: A pointer to the destination string @dest.
  */
-char *_strcpy(char *dest, const char *src)
+char *_strcpy(char *dest, char *src)
 {
 	int index = 0;
 
@@ -17,6 +17,5 @@ char *_strcpy(char *dest, const char *src)
 		dest[index] = src[index];
 		index++;
 	}
-
 	return (dest);
 }
